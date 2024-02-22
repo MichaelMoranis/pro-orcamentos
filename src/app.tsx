670431f7c27ed1843d5aa96cc208bf9474c74df2
michaelmoranis <michaelmoranesul@gmail.com>
2024-02-21 23:04:10 -0300

    use react query with cache by tags

--- src/app.tsx
+++ src/app.tsx
@@ -5,9 +5,49 @@ import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
+import { useQuery, keepPreviousData } from '@tanstack/react-query'
 import { Pagination } from './components/pagination'
+import { useSearchParams } from 'react-router-dom'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
+
 
 export function App() {
+  const [searchParams] = useSearchParams()
+
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1 
+
+  const {data: tagResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', page],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10`)
+      const data = await response.json()
+      console.log(data)
+
+      return data
+    },
+    placeholderData: keepPreviousData,
+    staleTime: 1000 * 60,
+  })
+
+  if(isLoading) {
+    return null
+  }
 
   return (
     <div className='py-10 space-y-8'>
@@ -43,18 +83,18 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((value, index) => {
+            {tagResponse?.data.map((tag) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className='flex flex-col gap-0.5'>
-                      <span className='font-medium'>React</span>
-                      <span className='text-xs text-zinc-500'>2342-2323-4564-2342gr</span>
+                      <span className='font-medium'>{tag.title}</span>
+                      <span className='text-xs text-zinc-500'>{tag.id}</span>
                     </div>
                   </TableCell>
                   <TableCell className='text-zinc-300'>
-                    13 video(s)
+                    {tag.amountOfVideos} video(s)
                   </TableCell>
                   <TableCell className='text-right'>
                     <Button className='icon'>
@@ -66,7 +106,7 @@ export function App() {
             })}
           </TableBody>
         </Table>
-        <Pagination items={items} page={page} pages={pages} />
+        {tagResponse && <Pagination pages={tagResponse.pages} items={tagResponse.items} page={page} />}
       </main>
     </div>
   )
